7ee718a6754755014df46f97c51e93bf0e0c9147
Adeel Ahmed <leedademha@gmail.com>
2023-06-05 19:52:37 -0700

    Rectangle Sub

--- src/include/GenericTraits.h
+++ src/include/GenericTraits.h
@@ -9,7 +9,8 @@
 /// the head of all structs.
 /// This struct demostrates how to do OOP in C, which is similar to v-table in
 /// C++.
-typedef struct GenericTraits {
+typedef struct GenericTraits
+{
 
   void *(*new)(char *self);
 
@@ -36,20 +37,25 @@ Destructor get_destructor(char *type_name);
 /// This function runs before main.
 void add_ctor_dtor(char *type_name, Constructor ctor, Destructor dtor);
 
-#define DECLARE_STRUCT(Object, Internal)                                       \
-  typedef struct Object Internal Object;                                       \
-  void *new_##Object(char *);                                                  \
-  void dump_##Object(void *, FILE *fp);                                        \
-  int cmp_##Object(void *, void *);                                            \
-  void drop_##Object(void *);                                                  \
-                                                                               \
+#define DECLARE_STRUCT(Object, Internal) \
+  typedef struct Object Internal Object; \
+  void *new_##Object(char *);            \
+  void dump_##Object(void *, FILE *fp);  \
+  int cmp_##Object(void *, void *);      \
+  void drop_##Object(void *);            \
+                                         \
   void __attribute__((constructor)) register_##Object();
 
-#define DEFINE_STRUCT(Object)                                                  \
-  void drop_##Object(void *this) { /* Your code here to free the instance. */  \
-  }                                                                            \
-  void __attribute__((constructor)) register_##Object() {                      \
-    /* Your code here to register an object. */                                \
+#define DEFINE_STRUCT(Object)                            \
+  void drop_##Object(void *this)                         \
+  {                                                      \
+    Object *i = (Object *)this;                          \
+    free(i->impl);                                       \
+    free(i);                                             \
+  }                                                      \
+  void __attribute__((constructor)) register_##Object()  \
+  {                                                      \
+    add_ctor_dtor(#Object, new_##Object, drop_##Object); \
   }
 
 #endif
--- src/lib/Rectangle.c
+++ src/lib/Rectangle.c
@@ -5,3 +5,60 @@
 #include <string.h>
 
 /* Your code here to define the struct */
+
+void *new_Rectangle(char *input)
+{
+    Rectangle *new_rectangle = malloc(sizeof(Rectangle));
+    new_rectangle->impl = malloc(sizeof(GenericTraits));
+
+    new_rectangle->impl->new = new_Rectangle;
+    new_rectangle->impl->dump = dump_Rectangle;
+    new_rectangle->impl->cmp = cmp_Rectangle;
+    new_rectangle->impl->drop = drop_Rectangle;
+
+    if (input != NULL)
+    {
+        char *height = strtok(input, " ");
+        char *width = strtok(NULL, " ");
+        if (height != NULL && width != NULL)
+        {
+            new_rectangle->height = atoi(height);
+            new_rectangle->width = atoi(width);
+        }
+    }
+
+    return new_rectangle;
+}
+
+void dump_Rectangle(void *self, FILE *fp)
+{
+    Rectangle *this_rectangle = (Rectangle *)self;
+    fprintf(fp, "%d %d\n", this_rectangle->height, this_rectangle->width);
+}
+
+int cmp_Rectangle(void *self, void *other)
+{
+    Rectangle *rect1 = (Rectangle *)self;
+    Rectangle *rect2 = (Rectangle *)other;
+
+    int area1 = rect1->height * rect1->width;
+    int area2 = rect2->height * rect2->width;
+
+    if (area1 != area2)
+    {
+        return area1 - area2;
+    }
+    else
+    {
+        if (rect1->height != rect2->height)
+        {
+            return rect1->height - rect2->height;
+        }
+        else
+        {
+            return rect1->width - rect2->width;
+        }
+    }
+}
+
+DEFINE_STRUCT(Rectangle)
